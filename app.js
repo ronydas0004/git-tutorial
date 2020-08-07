8eac9d0692841fd77e00906778143395583d0a4c
ronydas0004 <ronydas3030@gmail.com>
2020-08-07 12:05:44 +0600

    merge

--- app.js
+++ app.js
@@ -2,12 +2,7 @@
 function demo(){
     console.log('one line')
     console.log('two line of code')
-<<<<<<< HEAD
-    console.log('three line of code')
-
-=======
     console.log('branch a')
->>>>>>> branchA
    
 }
 demo()      
