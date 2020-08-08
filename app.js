2d4d71130a93cc724d60666bf1269b58174fecfb
ronydas0004 <ronydas3030@gmail.com>
2020-08-08 13:14:06 +0600

    deleted previous method

--- app.js
+++ app.js
@@ -7,5 +7,7 @@ function demo(){
     console.log('branch a')
     console.log('branchA is change')
     console.log('now i am stay on vs code visual code')
+
+     console.log('i create visual code studio called vs code')
 }
 demo()      
