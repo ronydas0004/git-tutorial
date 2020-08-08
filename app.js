50170edd75e1122f80183185a4bc353040e00e84
ronydas0004 <ronydas3030@gmail.com>
2020-08-08 13:18:09 +0600

    branch change

--- app.js
+++ app.js
@@ -4,10 +4,6 @@ function demo(){
 
     console.log('two line of code')
     console.log('master branch')
-    console.log('branch a')
-    console.log('branchA is change')
-    console.log('now i am stay on vs code visual code')
-
-     console.log('i create visual code studio called vs code')
+    console.log('master branch')
 }
 demo()      
