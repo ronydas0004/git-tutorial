9299e1489b941c0b6f7905ec7fd34e776f5edd50
ronydas0004 <ronydas3030@gmail.com>
2020-08-08 13:17:26 +0600

    deleted code

--- app.js
+++ app.js
@@ -1,13 +1,6 @@
 
 function demo(){
-    console.log('one line')
-
-    console.log('two line of code')
-    console.log('master branch')
-    console.log('branch a')
-    console.log('branchA is change')
+ 
     console.log('now i am stay on vs code visual code')
-
-     console.log('i create visual code studio called vs code')
 }
 demo()      
